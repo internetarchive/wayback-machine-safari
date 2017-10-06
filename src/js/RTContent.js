917360b4981976cbe35a3ab5d346905ac8fe38d1
Eagle19243 <eagle19243@gmail.com>
2017-10-06 15:18:26 +0300

    - Remove previous Site Map modal and recreate when click "Site Map" button

--- src/js/RTContent.js
+++ src/js/RTContent.js
@@ -2,43 +2,42 @@
 function displayRTContent() {
     if (window.top !== window) return;
     
+    if (document.getElementById("myModal") != null) {
+        document.getElementById("myModal").remove();
+    }
+    
     var modal=document.createElement('div');
     modal.setAttribute('id','myModal');
-    if (document.getElementById("myModal") != null) {
-        document.getElementById("myModal").style.display = "block";
-    } else {
-        modal.setAttribute('class','RTmodal');
-        
-        var modalContent=document.createElement('div');
-        modalContent.setAttribute('class','modal-content');
-        var span=document.createElement('button');
-        var divBtn=document.createElement('div');
-        divBtn.setAttribute('id','divBtn');
-        var message=document.createElement('div');
-        message.setAttribute('id','message');
-        
-        span.innerHTML='&times';
-        span.setAttribute('class','RTclose');
+    modal.setAttribute('class','RTmodal');
+    
+    var modalContent=document.createElement('div');
+    modalContent.setAttribute('class','modal-content');
+    var span=document.createElement('button');
+    var divBtn=document.createElement('div');
+    divBtn.setAttribute('id','divBtn');
+    var message=document.createElement('div');
+    message.setAttribute('id','message');
+    
+    span.innerHTML='&times';
+    span.setAttribute('class','RTclose');
 
-        var main=document.createElement('div');
-        var sequence=document.createElement('div');
-        var chart=document.createElement('div');
-        sequence.setAttribute('id','sequence');
-        chart.setAttribute('id','chart');
-        main.setAttribute('id','main');
-        
-        modal.appendChild(divBtn);
-        modal.appendChild(span);
-        modal.appendChild(sequence);
-        modal.appendChild(chart);
-        modal.appendChild(message);
-        document.body.appendChild(modal);
-        
-        modal.style.display = "block";
-           
-        span.onclick = function() {
-            modal.style.display = "none";
-            modal.remove();
-        }
+    var main=document.createElement('div');
+    var sequence=document.createElement('div');
+    var chart=document.createElement('div');
+    sequence.setAttribute('id','sequence');
+    chart.setAttribute('id','chart');
+    main.setAttribute('id','main');
+    
+    modal.appendChild(divBtn);
+    modal.appendChild(span);
+    modal.appendChild(sequence);
+    modal.appendChild(chart);
+    modal.appendChild(message);
+    document.body.appendChild(modal);
+    
+    modal.style.display = "block";
+       
+    span.onclick = function() {
+        modal.style.display = "none";
     }
 }
